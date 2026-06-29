aee4dbf0155b89bbe6ebd4637fe65875dbf969fe
Fic-Luizmario.DC-2B-2026 <mozinhaabc@gmail.com>
2026-06-29 00:07:48 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,23 @@
+const visor = document.getElementById('caixa');
+
+function adicionarValor(a) {
+    visor.value += a;
+    console.log(visor.value);
+}
+
+function limparVisor() {
+    visor.value = ' ';
+}
+
+function apagar() {
+    visor.value = visor.value.slice(0, -1);
+}
+
+function calcularResultado() {
+    try {
+        visor.value = eval(visor.value);
+    } catch (erro) {
+        visor.value = '';
+        alert('Expressão inválida. Por favor, insira uma expressão válida.');
+    }
+}
